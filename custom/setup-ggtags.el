44418d2768f6159db5a80c1183f35334feef01a0
seth <seth@localhost.localdomain>
2016-12-10 17:44:55 +0800

    set up

---
+++ custom/setup-ggtags.el
@@ -0,0 +1,22 @@
+(require 'ggtags)
+
+(ggtags-mode 1)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+              (ggtags-mode 1))))
+
+
+(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+(define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+(define-key ggtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+(define-key ggtags-mode-map (kbd "M-.") 'ggtags-find-tag-dwim)
+(define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
+(define-key ggtags-mode-map (kbd "C-c <") 'ggtags-prev-mark)
+(define-key ggtags-mode-map (kbd "C-c >") 'ggtags-next-mark)
+
+(provide 'setup-ggtags)
---
+++ init.el
@@ -0,0 +1,53 @@
+(require 'package)
+(setq package-archives '(("gnu"   . "http://elpa.zilongshanren.com/gnu/")
+	("melpa" . "http://elpa.zilongshanren.com/melpa/")))
+(package-initialize)
+
+(when (not package-archive-contents)
+    (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(add-to-list 'load-path "~/.emacs.d/custom")
+
+(require 'setup-general)
+(if (version< emacs-version "24.4")
+    (require 'setup-ivy-counsel)
+;;(require 'setup-helm)
+;;(require 'setup-helm-gtags)
+(require 'setup-ggtags))
+(require 'setup-cedet)
+(require 'setup-editing)
+
+
+
+;; function-args
+;; (require 'function-args)
+;; (fa-config-default)
+;; (define-key c-mode-map  [(tab)] 'company-complete)
+;; (define-key c++-mode-map  [(tab)] 'company-complete)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(auto-save-default nil)
+ '(custom-enabled-themes (quote (tango)))
+ '(electric-pair-mode t)
+ '(global-linum-mode t)
+ '(make-backup-files nil)
+ '(package-selected-packages
+   (quote
+    (sr-speedbar google-c-style company-c-headers zygospore ggtags yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
