4c330d9734764efe93f6e2dcb4b5145b3e00a92b
lightningkay <ew_34@sina.com>
2019-03-25 16:55:22 +0800

    delete useless files

--- login.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-account=test
-key=test
-
-curl --data "DDDDD=${account}&upass=${key}&v46s=1&v4ip=&f4serip=172.30.201.2&0MKKey=" "https://lgn.bjut.edu.cn" > login.html
-
--- login6.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-account=test
-key=test
-
-curl --data "DDDDD=${account}&upass=${key}&v46s=2&v4ip=&f4serip=172.30.201.2&0MKKey=" "https://lgn6.bjut.edu.cn" > login.html
-
